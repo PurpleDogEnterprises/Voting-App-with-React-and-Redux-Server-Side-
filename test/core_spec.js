9eddbc346e1405d779b0c7f6a961ced96b68844f
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-01-12 09:05:02 -0500

    Ending the vote

--- test/core_spec.js
+++ test/core_spec.js
@@ -90,6 +90,23 @@ describe('application logic', () => {
       );
     });
 
+    it('marks winner when just one entry left', () => {
+    const state = Map({
+      vote: Map({
+        pair: List.of('Trainspotting', '28 Days Later'),
+        tally: Map({
+          'Trainspotting': 4,
+          '28 Days Later': 2
+        })
+      }),
+      entries: List()
+    });
+    const nextState = next(state);
+    expect(nextState).to.equal(Map({
+      winner: 'Trainspotting'
+    }));
+  });
+
   describe('vote', () => {
 
     it('creates a tally for the voted entry', () => {
@@ -138,5 +155,4 @@ describe('application logic', () => {
   });
 
 });
-
 
